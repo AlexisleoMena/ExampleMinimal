8e271338f12dcdbaa010e0c803a8b14c9db7b3a0
alexisleomena <alexismena2690@gmail.com>
2023-05-16 22:39:46 -0300

    We create the TodoItemDTO file

--- ExampleMinimal/Program.cs
+++ ExampleMinimal/Program.cs
@@ -1,4 +1,5 @@
 using ExampleMinimal.Data;
+using ExampleMinimal.DTOs;
 using ExampleMinimal.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,7 +9,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
-var todoItems = app.MapGroup("/todoitems");
+RouteGroupBuilder todoItems = app.MapGroup("/todoitems");
 
 todoItems.MapGet("/", GetAllTodos);
 todoItems.MapGet("/complete", GetCompleteTodos);
@@ -21,33 +22,40 @@ app.Run();
 
 #region Controller Methods
 static async Task<IResult> GetAllTodos (TodoDb db) { 
-    return TypedResults.Ok(await db.Todos.ToListAsync()); 
+    return TypedResults.Ok(await db.Todos.Select(x => new TodoItemDTO(x)).ToListAsync()); 
 }
 
 static async Task<IResult> GetCompleteTodos(TodoDb db)
 {
-    return TypedResults.Ok(await db.Todos.Where(t => t.IsComplete).ToListAsync());
+    return TypedResults.Ok(await db.Todos.Where(t => t.IsComplete).Select(x => new TodoItemDTO(x)).ToListAsync());
 }
 
 static async Task<IResult> GetTodo(int id, TodoDb db)
 {
     return await db.Todos.FindAsync(id)
-        is Todo todo ? Results.Ok(todo) : Results.NotFound();
+        is Todo todo ? Results.Ok(new TodoItemDTO(todo)) : Results.NotFound();
 }
 
-static async Task<IResult> CreateTodo(Todo todo, TodoDb db)
+static async Task<IResult> CreateTodo(TodoItemDTO todoItemDTO, TodoDb db)
 {
-    db.Todos.Add(todo);
+    var todoItem = new Todo
+    {
+        IsComplete = todoItemDTO.IsComplete,
+        Name = todoItemDTO.Name,
+    };
+    db.Todos.Add(todoItem);
     await db.SaveChangesAsync();
-    return Results.Created($"/todoitems/{todo.Id}", todo);
+
+    todoItemDTO = new TodoItemDTO(todoItem);
+    return Results.Created($"/todoitems/{todoItemDTO.Id}", todoItemDTO);
 }
 
-static async Task<IResult> UpdateTodo(int id, Todo inputTodo, TodoDb db)
+static async Task<IResult> UpdateTodo(int id, TodoItemDTO todoItemDTO, TodoDb db)
 {
     var todo = await db.Todos.FindAsync(id);
     if (todo is null) return Results.NotFound();
-    todo.Name = inputTodo.Name;
-    todo.IsComplete = inputTodo.IsComplete;
+    todo.Name = todoItemDTO.Name;
+    todo.IsComplete = todoItemDTO.IsComplete;
 
     await db.SaveChangesAsync();
     return Results.NoContent();
@@ -59,7 +67,8 @@ static async Task<IResult> DeleteTodo(int id, TodoDb db)
     {
         db.Todos.Remove(todo);
         await db.SaveChangesAsync();
-        return Results.Ok(todo);
+        TodoItemDTO todoItemDTO = new TodoItemDTO(todo);
+        return Results.Ok(todoItemDTO);
     }
     return Results.NotFound();
 }
