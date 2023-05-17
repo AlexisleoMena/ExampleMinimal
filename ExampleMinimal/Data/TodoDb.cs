c35568b7c9d459d37f4e789ed0143d9800d73657
alexisleomena <alexismena2690@gmail.com>
2023-05-16 21:52:03 -0300

    Agregar archivos de proyecto.

---
+++ ExampleMinimal/Data/TodoDb.cs
@@ -0,0 +1,11 @@
+﻿using ExampleMinimal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExampleMinimal.Data
+{
+    public class TodoDb : DbContext
+    {
+        public DbSet<Todo> Todos => Set<Todo>();
+        public TodoDb(DbContextOptions<TodoDb> options) : base(options) { }
+    }
+}
